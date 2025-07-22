7a03dae1e64e389379487de1c8c62984c39422c7
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-07-22 15:41:47 -0500

    Set the capacity, load factor and hashmap function
    hashmap function already mods the hash code after
    it converts the key into a numerical number.
    After modding it will give us a index thats within
    our current "capacity" (bucket array size).

--- src/factories/hashmap.js
+++ src/factories/hashmap.js
@@ -1,9 +1,30 @@
+/* eslint-disable no-magic-numbers */
 export default function hashMap() {
-    let loadFactor;
-    let capcity;
+    // a factor to determine when to grow our buckets array
+    let loadFactor = 0.75;
+
+    // total buckets we currently have (usually starts with size 16)
+    let capacity = 16;
+
+    // note: starting with a capacity of 8 (a number that is in the power of ^2) to ensure that 
 
     function hello() {
         console.log("hello world!");
+    }
+
+    // takes a key input and produces a hash code, and then mods it to produce an index inside our hashmap buckets array
+    function hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i += 1) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+
+            // the odin project says to mod (%) on every iteration to keep it within valid integer range for javascript 
+            hashCode %= capacity; 
+        }
+
+        return hashCode;
     }
 
     // function set(key, value) {}
@@ -23,6 +44,6 @@ export default function hashMap() {
     // function values() {}
 
     // function entries() {}
-    
-    return { hello };
+
+    return { hello, hash };
 }
